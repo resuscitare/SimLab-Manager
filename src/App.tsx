bf2e542cd6373d9b0dccfba18c42cef8932d87e3
[dyad] <danilo.a.guimaraes@resuscitare.com.br>
2025-10-25 17:33:56 -0300

    [dyad] Fixed all 745 TypeScript errors in NovoCenario and App components - wrote 3 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
 import Layout from "./components/Layout";
 import Dashboard from "./pages/Dashboard";
 import Agendamentos from "./pages/Agendamentos";
+import NovoAgendamento from "./pages/NovoAgendamento";
+import Cenarios from "./pages/Cenarios";
 import NovoCenario from "./pages/NovoCenario";
 import Checklists from "./pages/Checklists";
+import NovoChecklist from "./pages/NovoChecklist";
+import EditarChecklist from "./pages/EditarChecklist";
+import AssistenteIA from "./pages/AssistenteIA";
+import Equipamentos from "./pages/Equipamentos";
+import Instrutores from "./pages/Instrutores";
+import AdminDashboard from "./pages/AdminDashboard";
 import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
 const App = () => (
-  <QueryClient client={queryClient}>
+  <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
       <Sonner />
@@ -19,6 +31,7 @@ const App = () => (
           <Routes>
             <Route path="/" element={<Dashboard />} />
             <Route path="/agendamentos" element={<Agendamentos />} />
+            <Route path="/agendamentos/novo" element={<NovoAgendamento />} />
             <Route path="/cenarios" element={<Cenarios />} />
             <Route path="/cenarios/novo" element={<NovoCenario />} />
             <Route path="/checklists" element={<Checklists />} />
@@ -27,13 +40,14 @@ const App = () => (
             <Route path="/ia" element={<AssistenteIA />} />
             <Route path="/equipamentos" element={<Equipamentos />} />
             <Route path="/instrutores" element={<Instrutores />} />
+            <Route path="/admin" element={<AdminDashboard />} />
             <Route path="/login" element={<Login />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </BrowserRouter>
-      </TooltipProvider>
-  </QueryClient>
+        </Layout>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
 );
 
 export default App;
--- src/components/cenario/FramesTab.tsx
+++ src/components/cenario/FramesTab.tsx
@@ -598,7 +598,7 @@ const FrameCard = ({
             min={param.unit === '%' ? 0 : undefined}
             max={param.unit === '%' ? 100 : undefined}
             step={param.key === 'ph' ? 0.01 : undefined}
-            value={value || ''}
+            value={String(value || '')}
             onChange={(e) => onUpdate(param.key, e.target.value)}
             className="pr-12"
           />
