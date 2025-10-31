9f1ecfc7c3408f4a66f95ac904d339848101e204
resuscitare <danilo.a.guimaraes@resuscutare.com.br>
2025-10-31 11:57:25 -0300

    [dyad] wrote 4 file(s)

--- src/pages/NovoCenario.tsx
+++ src/pages/NovoCenario.tsx
@@ -1,13 +1,10 @@
-import RevisaoTab from "../components/cenario/tabs/RevisaoTab";
-import DebriefingTab from "../components/cenario/tabs/DebriefingTab";
-
-const NovoCenario = () => {
-  // Component implementation
-  return (
-    <div>
-      {/* Component content */}
-    </div>
-  );
-};
-
-export default NovoCenario;
+<Badge
+                  variant={activeTab === tab.value ? "default" : tab.status === "ativo" ? "default" : "secondary"}
+                  className={`cursor-pointer ${
+                    activeTab === tab.value ? "bg-primary" : 
+                    tab.status === "ativo" ? "bg-green-500" : ""
+                  }`}
+                  onClick={() => handleTabChange(tab.value)}
+                >
+                  {tab.label}
+                </Badge>
